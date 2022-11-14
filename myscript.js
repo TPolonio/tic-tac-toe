d592821234308cf96f4e0e61176647b60ab70c29
TPolonio <tpolonio@live.com.pt>
2022-11-14 00:48:46 +0000

    Working on turn cycle

---
+++ myscript.js
@@ -0,0 +1,12 @@
+const gameBoard = document.querySelector('.game-board');
+const gameSquares = document.querySelectorAll('.square');
+let player1 = 'X'
+let player2 = 'O'
+let currentPlayer = player1;
+
+
+            if (currentPlayer = player1) {
+                currentPlayer == player2;   
+            } if (currentPlayer == player2) {
+                currentPlayer == player1; 
+            }

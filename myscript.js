5ad2ce9069271689fd01a1c9645dac38cfae6f2f
TPolonio <tpolonio@live.com.pt>
2022-11-19 14:37:15 +0000

    Added Win Condition

--- myscript.js
+++ myscript.js
@@ -1,12 +1,39 @@
 const gameBoard = document.querySelector('.game-board');
 const gameSquares = document.querySelectorAll('.square');
-let player1 = 'X'
-let player2 = 'O'
-let currentPlayer = player1;
+const winnerText = document.querySelector('.winner')
+let currentPlayer = 'X';
+let boardOptions = ['', '', '', '', '', '', '', '', '',];
+let running = false;
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
 
-            if (currentPlayer = player1) {
-                currentPlayer == player2;   
-            } if (currentPlayer == player2) {
-                currentPlayer == player1; 
-            }
+
+//Place Mark
+
+gameSquares.forEach((gameSquare) => {
+    gameSquare.addEventListener('click', () => {
+        if (gameSquare.innerText != '') return;
+            gameSquare.innerText = currentPlayer;
+            checkWinner();
+            currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
+       } 
+     )
+  })
+
+function checkWinner() {
+    winningCombinations.forEach(combination => {
+        let check = combination.every(i => gameSquares[i].innerText.trim() == currentPlayer);
+        if (check) {
+            winnerText.innerText = (`${currentPlayer} has won`)
+        }
+    });
+  }

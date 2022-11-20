49ea998b962b26aabcc15ed2bae6b7f9bc934b91
TPolonio <tpolonio@live.com.pt>
2022-11-20 02:41:25 +0000

    Finished project

--- myscript.js
+++ myscript.js
@@ -1,9 +1,11 @@
 const gameBoard = document.querySelector('.game-board');
 const gameSquares = document.querySelectorAll('.square');
-const winnerText = document.querySelector('.winner')
+const winnerText = document.querySelector('.winner-text')
+const restartButton = document.querySelector('.restart-button')
 let currentPlayer = 'X';
 let boardOptions = ['', '', '', '', '', '', '', '', '',];
-let running = false;
+let moveCounter = 0;
+let gameOver = false
 const winningCombinations = [
     [0, 1, 2],
     [3, 4, 5],
@@ -17,23 +19,41 @@ const winningCombinations = [
 
 
 
-//Place Mark
-
+//Play Round
 gameSquares.forEach((gameSquare) => {
     gameSquare.addEventListener('click', () => {
         if (gameSquare.innerText != '') return;
+        if(gameOver == true) return;
             gameSquare.innerText = currentPlayer;
             checkWinner();
             currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
+            moveCounter++
+            if (moveCounter == 9) { 
+                winnerText.innerText = 'Its a tie'
+            }
        } 
      )
   })
+
+//Restart Game
+restartButton.addEventListener('click',() =>{
+    restartGame();
+})
 
 function checkWinner() {
     winningCombinations.forEach(combination => {
         let check = combination.every(i => gameSquares[i].innerText.trim() == currentPlayer);
         if (check) {
-            winnerText.innerText = (`${currentPlayer} has won`)
+            winnerText.innerText = (`${currentPlayer} has won`);
+            gameOver = true;
         }
     });
   }
+
+function restartGame() {
+    gameSquares.forEach((gameSquare) => {
+        gameSquare.innerText = '';
+        winnerText.innerText = '';
+        gameOver = false;
+        moveCounter = 0
+})}
